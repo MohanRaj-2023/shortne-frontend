2d39c06cab9e2b1b29c773f0d63e7d9e721a7c9d
unknown <mohanraj.ns2020@gmail.com>
2025-07-14 11:48:59 +0530

    added delete account feature

--- src/Api/api.jsx
+++ src/Api/api.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 // render : 'https://shortne-backend.onrender.com/api/'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://shortne-backend.onrender.com/api/',
 });
 
 // Interceptor
